f816abd818a6603dfe7fb5a300ea7186bb6cfe8f
Valentina <moskwitina.v@yandex.ru>
2021-11-12 17:32:58 +0300

    image
    change

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div class='container pt-1'>
      <div class='card'>
-        <h2>Срочные новости на {{day}}</h2>
+        <h2>{{title}}</h2>
+       <img v-if="shouldCatShow" :src='cat'/>
+       <img v-else :src='dog'/>
      </div>
   </div>
-     <AppNews v-for="item in news" :key="item" :title="item"/>
+     <AppNews 
+       @cat="catHandler"
+       @dog="dogHandler"
+       />
    
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
 import AppNews from './components/AppNews.vue'
+import Cat from '@/img/cat.jpg'
+import Dog from '@/img/dog.jpg'
 export default {
   data(){
     return {
-               open: false,
-               day: new Date().toLocaleDateString(),
-               news: [
-                   'Новость 1',
-                   'Новость 2',
-                   'Новость 3',
-                   'Новость 4'
-               ]
+      title: 'Изменить картинку',
+      cat: Cat,
+      dog: Dog,
+      shouldCatShow: true, 
     }
   },
+  methods:{
+      catHandler(){
+        this.shouldCatShow = true;
+      },
+      dogHandler(){
+        this.shouldCatShow = false;
+      }
+    
+  },
   components: {
-    'the-header': TheHeader,
-    AppNews: AppNews
+    AppNews
   }
 }
 </script>
